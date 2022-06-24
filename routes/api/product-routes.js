5cb96c5b0efe2d23c1159e2a7863a4227d38726a
John Boi <jon0908776@gmail.com>
2022-06-24 18:31:48 -0400

    Bugfix

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,18 +111,18 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const tagData = await Tag.destroy({
+    const productData = await Product.destroy({
       where: {
         id: req.params.id,
       },
     });
 
-    if (!tagData) {
-      res.status(404).json({ message: 'No Tag found with that id!' });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
       return;
     }
 
-    res.status(200).json({ message: 'Tag deleted successfully.' });
+    res.status(200).json({ message: 'Product deleted successfully.' });
   } catch (err) {
     res.status(500).json(err);
   }
